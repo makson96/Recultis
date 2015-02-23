b6997dd8f6d645d0e6b25e09b8f46081731790f8
Tomasz Makarewicz <makson96@gmail.com>
2015-02-23 19:52:48 +0100

    initial source

---
+++ free_engineer.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+
+import sys, os
+from subprocess import check_output, CalledProcessError, call#, PIPE
+
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+
+games = [ ["Jedi Knight: Jedi Academy on OpenJK engine", "openjk"]]
+
+class Window(QWidget):
+	
+	game_nr = 0
+	engine_installed = 0
+	
+	def __init__(self, parent=None):
+		super(Window, self).__init__(parent)
+		
+		nameLabel = QLabel("Choose the game to install:")
+		game_group = QButtonGroup()
+		self.r0 = QRadioButton(games[0][0])
+		game_group.addButton(self.r0)
+		#self.r1 = QRadioButton("1")
+		#game_group.addButton(self.r1)
+		self.r0.setChecked(True)
+		self.chooseButton = QPushButton("Choose")
+		self.exitButton = QPushButton("Exit")
+		
+		vbox1 = QVBoxLayout()
+		hbox1 = QHBoxLayout()
+
+		vbox1.addWidget(nameLabel)
+		vbox1.addWidget(self.r0)
+		#vbox1.addWidget(self.r1)
+		hbox1.addWidget(self.chooseButton)
+		hbox1.addWidget(self.exitButton)
+		vbox1.addLayout(hbox1)
+		
+		self.chooseButton.clicked.connect(self.choose)
+		self.exitButton.clicked.connect(self.close)
+ 
+		mainLayout = QGridLayout()
+		mainLayout.addLayout(vbox1, 0, 1)
+		
+		self.setLayout(mainLayout)
+		self.setWindowTitle("Free Engineer")
+	
+	def choose(self):
+		if self.r0.isChecked():
+			self.game_nr = 0
+		
+		try:
+			check_dep = check_output("dpkg-query -W --showformat='${Status}\n' " + games[self.game_nr][1], shell=True)
+			result = str(check_dep)#.split(" ")
+			print(result)
+			result = result[2:-3]
+		except CalledProcessError as e:
+			print(e.returncode)
+			result = "non-installed1"
+		print(result)
+		if result != "install ok installed":
+			dep_pack = QMessageBox.question(self, "Install Engine", "You need to install following packages (root password required):<br><b>" + games[self.game_nr][1] +"</b><br>Continue?", QMessageBox.Yes | QMessageBox.No, QMessageBox.Yes)
+			if dep_pack == QMessageBox.Yes:
+				print("Yes")
+				dep_install = call("""x-terminal-emulator -e 'gksudo "apt-get -y install """  + games[self.game_nr][1] + """"'""", shell=True)
+				self.engine_installed = 1
+			else:
+				print("No")
+				self.engine_installed = 0
+		else:
+			self.engine_installed = 1
+
+		if self.engine_installed == 1:
+			if self.game_nr == 0:
+				import openjk as game_data
+			download_data = game_data.Steam(self, 1)
+			self.close()
+				
+			
+
+if __name__ == '__main__':
+	app = QApplication(sys.argv)
+	screen = Window()
+	screen.show()
+	sys.exit(app.exec_())
---
+++ openjk.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+
+import sys, os, tarfile, time
+from subprocess import call, check_output
+
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+
+self_dir = "/opt/free-engineer/"
+engine_dir = "/opt/OpenJK/"
+local_openjk_dir = os.getenv("HOME") + "/.local/share/openjk/base/"
+#manifest_dir = os.getenv("HOME") + "/.steam/steam/SteamApps/"
+
+engineer_dir = os.getenv("HOME") + "/.free-engineer/"
+game_data_dir = engineer_dir + "JediAcademy/GameData/"
+s_appid = "6020"
+
+def steamcmd(user):
+	if os.path.isdir(engineer_dir) == False:
+		os.makedirs(engineer_dir)
+	os.chdir(engineer_dir)
+	if os.path.isfile(engineer_dir+"steamcmd.sh") == False:
+		tar = tarfile.open(self_dir + "steamcmd_linux.tar.gz")
+		tar.extractall()
+		tar.close()
+	print(user)
+	if os.path.isdir(engineer_dir + "JediAcademy/") == False:
+		s_download = call("x-terminal-emulator -e './steamcmd.sh +@sSteamCmdForcePlatformType windows +login " + user + " +force_install_dir " + engineer_dir + "JediAcademy/ +app_update " + s_appid + " validate +quit'", shell=True)
+	while os.path.isdir(game_data_dir) == False:
+		time.sleep(2)
+	symlink()
+
+def symlink():
+	for binary in next(os.walk(engine_dir))[2]:
+		if os.path.exists(game_data_dir + binary) == False:
+			print("symlinking " + binary)
+			os.symlink(engine_dir + binary, game_data_dir + binary)
+		if os.path.isdir(local_openjk_dir) == False:
+			os.makedirs(local_openjk_dir)
+		for library in next(os.walk(engine_dir + "OpenJK/"))[2]:
+			if os.path.exists(local_openjk_dir + library) == False:
+				print("symlinking " + library)
+				os.symlink(engine_dir + "OpenJK/" + library, local_openjk_dir + library)
+	launchers()
+
+def launchers():
+	print("make_launchers")
+	desk_dir = str(check_output(['xdg-user-dir', 'DESKTOP']))[2:-3]
+	if os.uname()[4] == "x86_64":
+		print("symlinking desktop amd64")
+		os.symlink(self_dir + "openjk_amd64_sp.desktop", desk_dir + "/openjk_amd64_sp.desktop")
+		os.symlink(self_dir + "openjk_amd64_sp.desktop", os.getenv("HOME") + "/.local/share/applications/openjk_amd64_sp.desktop")
+		os.symlink(self_dir + "openjk_amd64_mp.desktop", desk_dir + "/openjk_amd64_mp.desktop")
+		os.symlink(self_dir + "openjk_amd64_mp.desktop", os.getenv("HOME") + "/.local/share/applications/openjk_amd64_mp.desktop")
+	else:
+		print("symlinking desktop i386")
+		os.symlink(self_dir + "openjk_i386_sp.desktop", desk_dir + "/openjk_i386_sp.desktop")
+		os.symlink(self_dir + "openjk_i386_sp.desktop", os.getenv("HOME") + "/.local/share/applications/openjk_i386_sp.desktop")
+		os.symlink(self_dir + "openjk_i386_mp.desktop", desk_dir + "/openjk_i386_mp.desktop")
+		os.symlink(self_dir + "openjk_i386_mp.desktop", os.getenv("HOME") + "/.local/share/applications/openjk_i386_mp.desktop")
+		
+	msgBox = QMessageBox.information(qw, "Game is ready", "Have fun!")#, QMessageBox.OK, QMessageBox.OK)
+	#msgBox.setText("Game is ready")
+	#msgBox.setInformativeText("Have fun!")
+	#msgBox.setStandardButtons(QMessageBox.OK)
+	#msgBox.setDefaultButton(QMessageBox.OK)
+	#ret = msgBox.exec_()
+	#if msgBox == QMessageBox.OK:
+	qw.close()
+
+class Steam:
+	
+	nested = 0
+	
+	def __init__(self, rootWindow = 0, nested = 0):
+		global qw
+		self.nested = nested
+		if self.nested == 0:
+			s_app = QApplication(sys.argv)
+		#tutaj okno z wyborem skad pobrac game data
+		nameLabel = QLabel("Choose the platfrom from which you want to get game data:")
+		p_group = QButtonGroup()
+		r0 = QRadioButton("Steam")
+		p_group.addButton(r0)
+		#self.r1 = QRadioButton("1")
+		#p_group.addButton(r1)
+		r0.setChecked(True)
+		loginLabel = QLabel("Enter your Steam login:")
+		loginText = QLineEdit()
+		chooseButton = QPushButton("Choose")
+		exitButton = QPushButton("Exit")
+		
+		vbox1 = QVBoxLayout()
+		hbox1 = QHBoxLayout()
+		hbox2 = QHBoxLayout()
+		qw = QWidget()
+
+		vbox1.addWidget(nameLabel)
+		vbox1.addWidget(r0)
+		#vbox1.addWidget(r1)
+		hbox1.addWidget(loginLabel)
+		hbox1.addWidget(loginText)
+		hbox2.addWidget(chooseButton)
+		hbox2.addWidget(exitButton)
+		vbox1.addLayout(hbox1)
+		vbox1.addLayout(hbox2)
+		#qw.setLayout(vbox1)
+		
+		chooseButton.clicked.connect(lambda : steamcmd(str(loginText.text())))
+		exitButton.clicked.connect(qw.close)
+ 
+		mainLayout = QGridLayout()
+		mainLayout.addLayout(vbox1, 0, 1)
+		
+		qw.setLayout(mainLayout)
+		qw.setWindowTitle("Choose Game Data Platform")
+		qw.show()
+		if self.nested == 0:
+			s_app.exec_()
+	
+#	def start_s(self, user):
+#		steamcmd(user)
+	
+	def manifest_and_steam(self):
+		print(manifest_dir)
+		manifest_file = manifest_dir + "appmanifest_6020.acf"
+		manifest = open(manifest_file,'w')
+		manifest.write(""""AppState"
+    {
+      "AppID"  "6020"
+      "Universe" "1"
+      "StateFlags" "1026"
+    }""")
+		manifest.close()
+		os.chmod(manifest_file, stat.S_IRUSR | stat.S_IRGRP | stat.S_IROTH)
+		#p_steam = call("steam  -applaunch 6020", executable="/bin/bash", shell=True)
+		p_steam = call("steam", executable="/bin/bash", shell=True)
+		print("steam closed")
+		os.remove(manifest_dir + "appmanifest_6020.acf")
+		#symlink()
