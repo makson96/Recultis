ee91c25a400b0983b16a924413a4b14e050cd096
Tomasz Makarewicz <makson96@gmail.com>
2015-04-07 13:30:38 +0200

    license program to gpl3

--- steam.py
+++ steam.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 #-*- coding: utf-8 -*-
 
+##This software is available to you under the terms of the GPL-3, see "/usr/share/common-licenses/GPL-3".
+##Copyright:
+##- Tomasz Makarewicz (makson96@gmail.com)
+
 import os, tarfile, urllib.request
 from subprocess import call
 
--- xcom.py
+++ xcom.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 #-*- coding: utf-8 -*-
 
-import sys, os, tarfile, time, shutil
-from subprocess import call, check_output
+##This software is available to you under the terms of the GPL-3, see "/usr/share/common-licenses/GPL-3".
+##Copyright:
+##- Tomasz Makarewicz (makson96@gmail.com)
+
+import sys, os, time, shutil
+from subprocess import check_output
 
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
