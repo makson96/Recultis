efb2f38ded8552bb95a70ce5ce4aee318486c552
Tomasz Makarewicz <makson96@gmail.com>
2016-10-08 13:23:20 +0200

    remove unsupported files

--- gog.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-#-*- coding: utf-8 -*-
-
-##This software is available to you under the terms of the GPL-3, see "/usr/share/common-licenses/GPL-3".
-##Copyright:
-##- Tomasz Makarewicz (makson96@gmail.com)
-
-#Support for the gog using lgogdownloader
