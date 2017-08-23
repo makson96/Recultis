9045764bec646d3ea3243fa767116388fd0d3b4e
Tomasz Makarewicz <makson96@gmail.com>
2017-08-23 13:18:07 +0200

    handle situation, when cannot execute steamcmd

--- tools/steam.py
+++ tools/steam.py
@@ -11,23 +11,23 @@ from subprocess import Popen, PIPE
 def start(login, password, recultis_dir, s_appid, game_dir):
 	os.chdir(recultis_dir)
 	steamcmd_install(recultis_dir)
-	steam_error = 1
+	steam_error = 2
 	retry_nr = 0
-	while steam_error == 1:
+	while steam_error == 2:
 		steam_error = run(login, password, recultis_dir, s_appid, game_dir)
-		if steam_error == 1:
+		if steam_error == 2:
 			print("Steamcmd error. Retry.")
 			retry_nr = retry_nr + 1
 			if retry_nr == 5:
 				print("Steamcmd error. Reinstall steamcmd.")
 				steamcmd_reinstall(recultis_dir)
 			elif retry_nr == 8:
-				steam_log_file = open("steam_log.txt", "a")
-				steam_log_file.write("\nSteamcmd Error. Terminate.")
-				steam_log_file.close()
-				print("Steamcmd Error. Terminate.")
-				return 0
-	return 1
+				steam_error = 0
+	steam_log_file = open("steam_log.txt", "a")
+	steam_log_file.write("\nSteamcmd Error. Terminate.")
+	steam_log_file.close()
+	print("Steamcmd Error. Terminate.")
+	return steam_error
 
 def steamcmd_install(recultis_dir):
 	if os.path.isfile(recultis_dir+"steamcmd.sh") == False:
@@ -73,13 +73,18 @@ def run(login, password, recultis_dir, s_appid, game_dir):
 			steam_download.terminate()
 		#Retry 5 times if steamcmd has memory access error
 		elif '$DEBUGGER "$STEAMEXE" "$@"' in steam_last_line:
-			return 1
+			return 2
 		#If computer is not registered on Steam, handle Steam Guard
 		elif 'Steam Guard' in steam_last_line:
 			steam_guard_code = steam_guard(recultis_dir)
 			steam_download.stdin.write(bytes(steam_guard_code + '\n', 'ascii'))
 			steam_download.stdin.flush()
-	return 0
+	#if there is only 1 line after steamcmd finished working, it means it crashed.
+	if sum(1 for line in open('steam_log.txt')) == 1:
+		rc = 0
+	else:
+		rc = 1
+	return rc
 
 def steamcmd_reinstall(recultis_dir):
 	if os.path.isfile(recultis_dir+"steam.sh") == True:
