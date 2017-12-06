cc9e096b48f606e4e0b3cfe22003c5b70459d821
Tomasz Makarewicz <makson96@gmail.com>
2017-12-06 20:06:42 +0100

    bump version to 1.2.4pre

--- recultis.py
+++ recultis.py
@@ -15,7 +15,7 @@ from PyQt5.QtGui import *
 from tools import update_do, status
 from games import installer
 
-recultis_version = "1.2.3"
+recultis_version = "1.2.4pre"
 
 self_dir = os.path.dirname(os.path.abspath(__file__)) + "/"
 recultis_dir = os.getenv("HOME") + "/.recultis/"
